ec9027632da7f4bf5ccbdf1b6c1378d6280a0287
nachosan <acosmin.maria@gmail.com>
2019-11-02 19:39:56 +0200

    added models and dbcontext

---
+++ smarthack/Data/SmartHackDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using smarthack.Models;
+
+namespace smarthack.Data
+{
+    public class SmartHackDbContext : DbContext
+    {
+        public SmartHackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<News> Newses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //            optionsBuilder.UseMySQL(
+            //                "server=eu-cdbrwest-02.cleardb.net;Database=heroku_1c3f591c0e17d07;userid=b5977c722a69d6;password=a7c61ba3; port=3306");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ smarthack/Models/News.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace smarthack.Models
+{
+    public class News
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public string Headline { get; set; }
+        public string Link { get; set; }
+        public string Content { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+
+    }
+}
