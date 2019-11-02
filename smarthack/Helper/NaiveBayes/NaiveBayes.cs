265421380d63f62e95857332bd31cd5ef4eb7a72
Mihaela Tudor <mihaela.tudor@cst-dev.com>
2019-11-02 19:43:55 +0200

    added naive-bayes singleton-class

---
+++ smarthack/Helper/NaiveBayes/NaiveBayes.cs
@@ -0,0 +1,35 @@
+﻿using Accord.MachineLearning.Bayes;
+using Accord.Statistics.Filters;
+
+namespace smarthack.Helper.NaiveBayes
+{
+    public class NaiveBayes
+    {
+        private static NaiveBayes instance;
+        private static NaiveBayesLearning lerner;
+        private static NaiveBayes naiveBayes;
+        static Codification codebook;
+        private NaiveBayes()
+        {
+
+        }
+        public static NaiveBayes Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NaiveBayes();
+                    lerner = new NaiveBayesLearning();
+                }
+                return instance;
+            }
+        }
+        public void learnNaiveBayes()
+        {
+
+        }
+
+    }
+}
+
--- smarthack/Helper/SentimentAnalyzer/Clasifier/Clasifier.cs
+++ smarthack/Helper/SentimentAnalyzer/Clasifier/Clasifier.cs
@@ -2,10 +2,8 @@
 using Microsoft.ML.Data;
 using smarthack.Helper.SentimentAnalyzer.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using static Microsoft.ML.DataOperationsCatalog;
 
 namespace smarthack.Helper.Clasifier
@@ -19,10 +17,21 @@ namespace smarthack.Helper.Clasifier
             MLContext mlContext = new MLContext();
             TrainTestData splitDataView = LoadData(mlContext);
             ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
-            //Evaluate(mlContext, model, splitDataView.TestSet);
-            var resultPrediction = UseModelWithSingleItem(mlContext, model);
+            var predictionResult = UseModelWithSingleItem(mlContext, model);
         }
-
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(Clasifier._dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            var model = estimator.Fit(splitTrainSet);
+            return model;
+        }
         private static SentimentPrediction UseModelWithSingleItem(MLContext mlContext, ITransformer model)
         {
             PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
@@ -32,37 +41,10 @@ namespace smarthack.Helper.Clasifier
             };
             var resultPrediction = predictionFunction.Predict(sampleStatement);
             Console.WriteLine($"Sentiment: {resultPrediction.Sentiment} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
-            var sentiment = Convert.ToBoolean(resultPrediction.Prediction) ? "Toxic" : "Not Toxic";
-            Console.WriteLine(sentiment);
+            //var sentiment = Convert.ToBoolean(resultPrediction.Prediction) ? "Toxic" : "Not Toxic";
+            //Console.WriteLine(sentiment);
             return resultPrediction;
         }
-        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
-        {
-            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
-            IDataView predictions = model.Transform(splitTestSet);
-            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
-            Console.WriteLine();
-            Console.WriteLine("Model quality metrics evaluation");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
-            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
-            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
-            Console.WriteLine("=============== End of model evaluation ===============");
-        }
-
-        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
-        {
-            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
-            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
-            var model = estimator.Fit(splitTrainSet);
-            return model;
-        }
-
-        public static TrainTestData LoadData(MLContext mlContext)
-        {
-            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(Clasifier._dataPath, hasHeader: false);
-            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
-            return splitDataView;
-        }
+     
     }
 }
