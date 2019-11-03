19861a4d4371ddfdaa00e0e033868ed9a6445d35
nachosan <acosmin.maria@gmail.com>
2019-11-03 03:46:35 +0200

    controllers

---
+++ smarthack/Controllers/NewsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using smarthack.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smarthack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private SmartHackDbContext _context;
+        public NewsController(SmartHackDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("company/{companyId}")]
+        public async Task<IActionResult> GetNewsByCompany(Guid companyId)
+        {
+            var news = await _context.Newses.Where(x => x.CompanyId == companyId).ToListAsync();
+            return Ok(new
+            {
+                Items = news
+            });
+
+        }
+
+
+    }
+}
